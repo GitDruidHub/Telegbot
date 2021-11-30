010e9220bdf435750aa91ecf45806551ed6da746
GitDruidHub <pythondruid@gmail.com>
2021-11-30 13:28:43 +0300

    bot say "hello"

--- source/Bot/bot.py
+++ source/Bot/bot.py
@@ -1,8 +1,14 @@
 import config
 import telebot
+from requests import get
+
 
 bot = telebot.TeleBot(config.token)
 
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, "Привет, я подопытный бот, который кое что могу делать")
 
 @bot.message_handler(func=lambda message: True)
 def any_message(message):
@@ -12,7 +18,7 @@ def any_message(message):
 @bot.edited_message_handler(func=lambda message: True)
 def edit_message(message):
     bot.edit_message_text(chat_id=message.chat.id,
-                          text= "Сам {!s}".format(message.text),
+                          text="Сам {!s}".format(message.text),
                           message_id=message.message_id + 1)
 
 if __name__ == '__main__':
--- source/others.py
+++ source/others.py
@@ -4,7 +4,6 @@ owm = pyowm.OWM("5e195eaedcb783c7bf3a21d13e9d10f7")
 
 place = input("В каком городе/стране?: ")
 mgr = owm.weather_manager()
-
 # Search for current weather in place and get details
 observation = mgr.weather_at_place(place)
 w = observation.weather
